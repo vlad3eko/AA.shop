9321077d1accc7eaac1c9bea98473a0e19cd81e0
vlad3eko <vlad3eko@gmail.com>
2026-02-05 23:08:28 +0300

    create orders-page

--- app/components/checkout/checkoutClient.vue
+++ app/components/checkout/checkoutClient.vue
@@ -15,29 +15,35 @@
                class="border-accent border p-2" autofocus/>
         <Input v-model="form.address" id="address" name="address" defaultValue="" placeholder="Адрес"
                class="border-accent border p-2"/>
-        <div class="flex flex-col gap-2">
-          <p>Способ доставки:</p>
+        <div class="flex gap-10">
           <div>
-            <input v-model="form.delivery" type="radio" id="currier" name="deliveryVariant" value="Курьер"
-                   class="mr-2 cursor-pointer" checked/>
-            <label for="currier" class="cursor-pointer">Курьерская служба</label>
+            <p>Способ доставки:</p>
+            <div>
+              <input v-model="form.delivery" type="radio" id="currier" name="deliveryVariant" value="Курьер"
+                     class="mr-2 cursor-pointer" checked/>
+              <label for="currier" class="cursor-pointer">Курьерская служба</label>
+            </div>
+            <div class="mb-5">
+              <input v-model="form.delivery" type="radio" id="takeSelf" name="deliveryVariant" value="ПВЗ"
+                     class="mr-2 cursor-pointer">
+              <label for="takeSelf" class="cursor-pointer">Пункт самовывоза</label>
+            </div>
           </div>
-          <div class="mb-5">
-            <input v-model="form.delivery" type="radio" id="takeSelf" name="deliveryVariant" value="ПВЗ"
-                   class="mr-2 cursor-pointer">
-            <label for="takeSelf" class="cursor-pointer">Пункт самовывоза</label>
-          </div>
-        </div>
-        <p>Дата доставки:</p>
-        <div>
-          <div class="mb-5">
+          <div>
+            <div class="flex gap-2">
+              <p>Дата доставки:</p>
+              <input v-model="form.date"
+                     type="date"
+                     :min="dateToday"
+                     class="mr-2 cursor-pointer">
+            </div>
             <input type="checkbox" id="soonAsPossible" name="soonAsPossible" value="Ближайшая"
                    class="mr-2 cursor-pointer" checked>
             <label for="soonAsPossible" class="cursor-pointer">Ближайшая</label>
+
           </div>
-          <input v-model="form.date" type="date"
-                 class="mr-2 cursor-pointer">
         </div>
+
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="ghost">Отмена</Button>
@@ -66,9 +72,18 @@ import {
 import {Button} from "~/components/ui/button";
 import {Input} from "~/components/ui/input";
 import {useCheckoutStore} from "~/store/checkout.store";
-import {NativeSelect, NativeSelectOption} from "~/components/ui/native-select";
 
 const checkoutStore = useCheckoutStore()
+
+const dateToday = computed(() => {
+  const date = new Date()
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = String(date.getFullYear())
+
+  return `${year}-${month}-${day}`
+})
 
 const form = reactive({
   name: '',
@@ -76,8 +91,6 @@ const form = reactive({
   delivery: checkoutStore.checkout.delivery,
   date: checkoutStore.checkout.date
 })
-
-console.log('form', form.date)
 
 </script>
 
